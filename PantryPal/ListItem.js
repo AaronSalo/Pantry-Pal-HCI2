196516d4c6974cd71ac6fcb09928a1e0ec5a8688
kyl-dc <57549207+kyl-dc@users.noreply.github.com>
2022-04-03 13:42:12 -0500

    Adding comments

--- PantryPal/ListItem.js
+++ PantryPal/ListItem.js
@@ -13,32 +13,40 @@ export default function ListItem(props) {
       className={`${isChecked ? "listItem--active" : ""}`}
     >
       <label>
+        {/* Custom Checkbox 
+            referenced from:
+            https://dev.to/tomdohnal/custom-checkbox-in-react-animated-and-accessible-3jk9 
+        */}
         <input type="checkbox"
           onChange={() => {
             setIsChecked(!isChecked);
           }}
         />
         <svg
-        className={`checkbox ${isChecked ? "checkbox--active" : ""}`}
-        // checkbox code referenced from:
-        // https://dev.to/tomdohnal/custom-checkbox-in-react-animated-and-accessible-3jk9
-        aria-hidden="true"
-        viewBox="0 0 15 11"
-        fill="none"
+          // Checkbox
+          className={`checkbox ${isChecked ? "checkbox--active" : ""}`}
+          aria-hidden="true"
+          viewBox="0 0 15 11"
+          fill="none"
         >
           <path
+            // Checkmark Path 
             d="M1 4.5L5 9L14 1"
             strokeWidth="2"
             stroke={isChecked ? "#fff" : "none"}
           />
         </svg>
       </label>
+
+      {/* Item Name and Tags*/}
       <div className="middle">
         <div className="title">
           {props.product}
         </div>
           <div className="tagContainer" dangerouslySetInnerHTML={{__html: renderedTags}}/>
       </div>
+
+      {/* Item Quantity and Remove */}
       <div className="right">
         <div className="remove"><a href="javascript:void(0);" >X</a></div>
         <div className="quantity">{props.quantity}</div>
